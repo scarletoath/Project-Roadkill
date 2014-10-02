28cb20f42c2d31ba678259c214890cd0019c7405
scarletoath <dragonlss@hotmail.com>
2014-10-02 16:47:26 -0400

    Added PlayerController which uses encapsulated data from GameInput. Added SimulationInput to de-couple Tango and mouse/keyboard input. Created Player prefab and updated GameManager prefab. Updated GameInput to read data from the TangoSDK.

--- Assets/Script/Input/GameInput.cs
+++ Assets/Script/Input/GameInput.cs
@@ -1,7 +1,79 @@
 ﻿using Tango;
 using UnityEngine;
 
+public enum ButtonState {
+	None ,
+	Released ,
+	Pressed ,
+	HoldingDown ,
+}
+
 public class GameInput : Singleton<GameInput> {
+
+	/// <summary>
+	/// Stores interpreted pose information from the TangoSDK.
+	/// </summary>
+	public class PoseData {
+
+		private Vector3 _CurrentPosition = Vector3.zero;
+		private Quaternion _CurrentRotation = Quaternion.identity;
+
+		private Vector3 _PreviousPosition = Vector3.zero;
+		private Quaternion _PreviousRotation = Quaternion.identity;
+
+		private Vector3 _Velocity = Vector3.zero;
+
+		private ButtonState _ClickState = ButtonState.None;
+
+		public Vector3 Position {
+			get {
+				return _CurrentPosition;
+			}
+			set {
+				if ( CanUpdatePoseData ) {
+					_PreviousPosition = _CurrentPosition;
+					_CurrentPosition = value;
+
+					_Velocity = ( _CurrentPosition - _PreviousPosition ) / Time.deltaTime;
+				}
+			}
+		}
+
+		public Quaternion Rotation {
+			get {
+				return _CurrentRotation;
+			}
+			set {
+				if ( CanUpdatePoseData ) {
+					_PreviousRotation = _CurrentRotation;
+					_CurrentRotation = value;
+
+					// Clamp Y-rotation to prevent gimbal lock
+					Vector3 Euler = _CurrentRotation.eulerAngles;
+					if ( Euler.x > 180 && Euler.x - 360 < -Instance.MaxLookAngleY ) {
+						Euler.x = -Instance.MaxLookAngleY;
+					}
+					else if ( Euler.x < 180 && Euler.x > Instance.MaxLookAngleY ) {
+						Euler.x = Instance.MaxLookAngleY;
+					}
+					_CurrentRotation.eulerAngles = Euler;
+				}
+			}
+		}
+
+		public Vector3 Velocity {
+			get {
+				return _Velocity;
+			}
+		}
+
+	}
+
+	/// <summary>
+	/// Indicates whether pose data is updated if the structure's data is 
+	/// attempted to be changed.
+	/// </summary>
+	private static bool CanUpdatePoseData = false;
 
 	/// <summary>
 	/// The prefab to instantiate if the scene does not already contain the 
@@ -10,9 +82,23 @@ public class GameInput : Singleton<GameInput> {
 	public GameObject TangoPrefab;
 
 	/// <summary>
+	/// Indicates whether to use mouse and keyboard for input. It is 
+	/// automatically set if TangoSDK is not valid.
+	/// </summary>
+	public bool IsSimulation = false;
+
+	/// <summary>
+	/// The maximum vertical angle from looking straight ahead.
+	/// </summary>
+	public float MaxLookAngleY = 80.0f;
+
+	/// <summary>
 	/// A reference to the TangoApplication component.
 	/// </summary>
 	private TangoApplication TangoApp;
+	private VIOProvider.VIOStatus VIOStatus;
+
+	private SimulationInput SimulationInput;
 
 	protected override void Awake () {
 		base.Awake ();
@@ -33,6 +119,20 @@ public class GameInput : Singleton<GameInput> {
 
 			gameObject.SetActive ( false );
 		}
+
+		// Auto set simulation mode if Tango not available
+		// Tango init occurs during Instantiate->Awake, so if available, it is init-ed by now
+		if ( !TangoApp.Valid || !TangoApp.IsInitialized () ) {
+			IsSimulation = true;
+
+			SimulationInput = gameObject.GetComponent<SimulationInput> ();
+		}
+
+		// Disable cursor
+		Screen.showCursor = false;
+
+		// Initialize data structures
+		Pose = new PoseData ();
 	}
 
 	// Use this for initialization
@@ -42,7 +142,27 @@ public class GameInput : Singleton<GameInput> {
 
 	// Update is called once per frame
 	void Update () {
+		CanUpdatePoseData = true;
 
+		UpdatePoseData ();
+
+		CanUpdatePoseData = false;
+	}
+
+	public static PoseData Pose { get; private set; }
+
+	private void UpdatePoseData () {
+		if ( IsSimulation ) {
+			SimulationInput.GetLatestPose ( Pose );
+
+			return;
+		}
+
+		// Get data from Tango data providers
+		if ( VIOProvider.GetLatestPose ( ref VIOStatus ) ) {
+			Pose.Position = VIOStatus.translation;
+			Pose.Rotation = VIOStatus.rotation;
+		}
 	}
 
 }
---
+++ Assets/Script/Input/SimulationInput.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class SimulationInput : MonoBehaviour {
+
+	public float MouseSensitivity = 120.0f;
+	public float MoveSpeed = 3.0f;
+
+	public bool UseDrift = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public bool GetLatestPose ( GameInput.PoseData Pose ) {
+		float RotX , RotY;
+		Quaternion Rot = Pose.Rotation;
+		RotX = Pose.Rotation.eulerAngles.x - GetAxisValue ( "Mouse Y" ) * Time.deltaTime * MouseSensitivity;
+		RotY = Pose.Rotation.eulerAngles.y + GetAxisValue ( "Mouse X" ) * Time.deltaTime * MouseSensitivity;
+		Rot.eulerAngles = new Vector3 ( RotX , RotY , 0 );
+		Pose.Rotation = Rot;
+
+		Vector3 DirForward , DirRight , DirUp , Pos = Pose.Position;
+		Rot.eulerAngles = new Vector3 ( 0 , RotY , 0 );
+		DirForward = Rot * Vector3.forward;
+		DirRight = Rot * Vector3.right;
+		DirUp = Vector3.up;
+		Pos += GetAxisValue ( "Vertical" ) * DirForward * Time.deltaTime * MoveSpeed;
+		Pos += GetAxisValue ( "Horizontal" ) * DirRight * Time.deltaTime * MoveSpeed;
+		Pose.Position = Pos;
+
+		return true;
+	}
+
+	/// <summary>
+	/// Gets the value of an axis, including drift if UseDrift is <code>true</code>.
+	/// </summary>
+	/// <param name="AxisName">The name of the axis whose value to retrieve.</param>
+	/// <returns></returns>
+	private float GetAxisValue ( string AxisName ) {
+		if ( UseDrift ) {
+			return Input.GetAxis ( AxisName );
+		}
+		else {
+			return Input.GetAxisRaw ( AxisName );
+		}
+	}
+
+}
