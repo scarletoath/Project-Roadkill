d6391058f36112769774bdf6702f7148c07d4036
wertkh32 <virtwert@hotmail.com>
2014-10-02 18:02:53 -0400

    changed to caveman

---
+++ Assets/DeerAIScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeerAIScript : MonoBehaviour {
+	public GameObject caveman;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.collider.gameObject.name == "Caveman") {
+						Destroy (this.gameObject);
+
+			//caveman.transform.Find ("blood").gameObject.GetComponent<ParticleSystem> ().Stop();
+						caveman.transform.Find ("blood").gameObject.GetComponent<ParticleSystem> ().Play ();
+				}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 diff = this.transform.position - caveman.transform.position;
+		if (diff.magnitude < 10.0f) 
+		{
+			this.transform.position += diff.normalized * 0.1f; 
+		}
+	}
+}
