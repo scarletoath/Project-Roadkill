a505ca9246b0cb350889da956c2711e256cc1a63
wertkh32 <virtwert@hotmail.com>
2014-10-04 13:17:28 -0400

    placeholder depthfiles

---
+++ Assets/DepthWallScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DepthWallScript : MonoBehaviour {
+	Texture2D depthTex;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
