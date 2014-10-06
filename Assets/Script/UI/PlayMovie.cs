f367ba2cff99f2c8b6eca043897b9e7f0843272d
scarletoath <dragonlss@hotmail.com>
2014-10-06 19:46:29 -0400

    Added MovieTest scene to test credits movie.

---
+++ Assets/Script/UI/PlayMovie.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayMovie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Handheld.PlayFullScreenMovie ( "Credits.mp4" , Color.black , FullScreenMovieControlMode.CancelOnInput );
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+}
