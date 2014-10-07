b8efeb26f87a9a6dfc9f24b8bd9f28ef9519eb61
scarletoath <dragonlss@hotmail.com>
2014-10-06 20:41:36 -0400

    Finished MainMenuScene. - Added sounds, textures and updated UIAtlas. - Added Panel animations. - Removed deprecated textures.

--- Assets/Script/UI/MainMenuUI.cs
+++ Assets/Script/UI/MainMenuUI.cs
@@ -1,9 +1,10 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class MainMenuUI : MonoBehaviour {
 
 	public void OnStartClick () {
-		Application.LoadLevel ( "GameScene" );
+		StartCoroutine ( LoadLevelAfterSound () );
 	}
 
 	public void OnHelpClick () {
@@ -11,7 +12,23 @@ public class MainMenuUI : MonoBehaviour {
 	}
 
 	public void OnCreditsClick () {
+#if UNITY_ANDROID
+		Handheld.PlayFullScreenMovie ( "StarWars.mp4" , Color.black , FullScreenMovieControlMode.CancelOnInput );
+#endif
+	}
 
+	public void OnBackClick () {
+
+	}
+
+	public void OnTitlePlay () {
+		animation [ "MainMenu - Title" ].normalizedTime = 1;
+	}
+
+	private IEnumerator LoadLevelAfterSound () {
+		yield return new WaitForSeconds ( 0.330f );
+
+		Application.LoadLevel ( "GameScene" );
 	}
 
 }
